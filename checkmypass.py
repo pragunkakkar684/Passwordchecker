5b221a771711beab5d53f854f77c7fdf09eb48a9
Pragun <kakkarpragun.work@gmail.com>
2024-12-26 22:18:15 +0530

    added the code to add files as arguments

--- checkmypass.py
+++ checkmypass.py
@@ -1,6 +1,7 @@
 import requests
 import hashlib
 import sys
+import argparse
 
 def request_api_data(query): #gets all the hash passwords starting with same query
     url = f"https://api.pwnedpasswords.com/range/{query}"
@@ -25,12 +26,36 @@ def pwned_api_check(password): #to check if the password exists in API response,
     res = request_api_data(first5_char)
     return get_password_leaks_count(res,tail)
 
-def main(args):
-    for password in args:
-        count = pwned_api_check(password)
-        if count:
-            print(f"{password} was found {count} times... you should probably change your password!")
-        else:
-            print(f"{password} was NOT found. Carry on!")
+def main():
+    parser  = argparse.ArgumentParser(description="Check your password against a database of over 1.1 million have-i-been-pwned passwords")
+    parser.add_argument('--password',type=str,help = "Comma-seperated list of passwords to check",required=False)
+    parser.add_argument('--file',type=str,help = "File containing passwords to check",required=False)
 
-main(sys.argv[1:])
+    args = parser.parse_args()
+
+    if args.password:
+        for password in args.password.split(','):
+            count = pwned_api_check(password)
+            if count:
+                print(f"{password} was found {count} times... you should probably change your password!")
+            else:
+                print(f"{password} was NOT found. Carry on!")
+    elif args.file:
+        try:
+            with open(args.file,'r') as file:
+                passwords = [line.strip() for line in file.readlines()]
+                for password in passwords:
+                    count = pwned_api_check(password)
+                    if count:
+                        print(f"{password} was found {count} times... you should probably change your password!")
+                    else:
+                        print(f"{password} was NOT found. Carry on!")
+        except FileNotFoundError:
+            print("File not found")
+    else:
+        print("Error: You must provide either a list of passwords or a file containing passwords.")
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    main()
